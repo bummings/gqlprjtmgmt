15c2fade5eb7a0dc436e43c448460c8eeab17677
bummings <eabummings@gmail.com>
2022-06-21 13:31:59 -0400

    adds gql queries for client and projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,6 +4,7 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
 // Client type
@@ -17,15 +18,46 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Project type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    clients: {
+      type: GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // in place of a Mongoose function, we're using a high order array method to match the ID in args with the ID in the dummy data
         return clients.find((client) => client.id === args.id);
+      },
+    },
+    projects: {
+      type: GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // in place of a Mongoose function, we're using a high order array method to match the ID in args with the ID in the dummy data
+        return projects.find((project) => project.id === args.id);
       },
     },
   },
