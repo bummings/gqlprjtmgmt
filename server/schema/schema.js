7fe39bf04f87e0edf4fda41d3bb86abb92fdba9d
bummings <eabummings@gmail.com>
2022-06-21 12:13:13 -0400

    initial

---
+++ server/schema/schema.js
@@ -0,0 +1,36 @@
+const { projects, clients } = require("../sampleData");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLSchema,
+} = require("graphql");
+
+// Client type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // in place of a Mongoose function, we're using a high order array method to match the ID in args with the ID in the dummy data
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
