00358e96579524b7acb8473a608a6358132649e9
bummings <hello@bummings.io>
2022-06-23 18:52:39 -0400

    updates schema resolvers from dummy data file to mongo DB

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -43,32 +43,30 @@ const ProjectType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    clients: {
-      type: GraphQLList(ClientType),
-      resolve(parent, args) {
-        return clients;
-      },
-    },
-    client: {
-      type: ClientType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        // in place of a Mongoose function, we're using a high order array method to match the ID in args with the ID in the dummy data
-        return clients.find((client) => client.id === args.id);
-      },
-    },
     projects: {
       type: GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // in place of a Mongoose function, we're using a high order array method to match the ID in args with the ID in the dummy data
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id)
+      },
+    },
+    clients: {
+      type: GraphQLList(ClientType),
+      resolve(parent, args) {
+        return Client.find();
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Client.findById(args.id);
       },
     },
   },
