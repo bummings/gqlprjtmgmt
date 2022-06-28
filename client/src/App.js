0656f1853d26b74bf98a7e64c2ee913357487ace
bummings <hello@bummings.io>
2022-06-28 00:44:18 -0400

    fixes array in favicon array looking for pngs, imports bootstrap, sort of works in a navbar??

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
 import flamingo from "./components/assets/flamingo.jpg";
+import Header from "./components/Header";
 
 function App() {
   return (
   <div className="container">
+
+    <Header />
+  <div className="flamingoContainer">
     <img 
       src={flamingo}
       alt="a flamingo appears"
       className="flamingo" 
     />
     <h1 className="bruh">bruh</h1>
+  </div>
   </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import flamingo from './assets/flamingo.jpg'
+
+
+export default function Header() {
+  return (
+    <nav className="navbar bg-light p-0">
+        <div className="navbar">
+            <a href="/" className="navbar-brand">
+                <div className="d-flex">🦩 flamingoes</div>
+            </a>
+        </div>
+    </nav>
+  )
+}
