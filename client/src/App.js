e9dfd27019f2f82ddc3e71ac0c7be07a33d34ac6
bummings <hello@bummings.io>
2022-06-28 00:15:17 -0400

    you just spent another 30 minutes on flamingos

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import flamingo from "./components/assets/flamingo.jpg";
 
 function App() {
-  return <div className="App">bruh</div>;
+  return (
+  <div className="container">
+    <img 
+      src={flamingo}
+      alt="a flamingo appears"
+      className="flamingo" 
+    />
+    <h1 className="bruh">bruh</h1>
+  </div>
+  );
 }
 
 export default App;
