b9f4be9f69de82019cc3e6204deddd8a14220695
bummings <hello@bummings.io>
2022-06-27 23:55:44 -0400

    Revert "you have spent the better part of an hour applying pink outlines around flamingos"
    This reverts commit c4c1f0947864e91aff48a941f0d4d9ad3268ae25.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import flamingo from './components/assets/flamingo.jpg'
-
 function App() {
-  return <div className="container">
-    <img 
-      src={flamingo} 
-      alt="a flamingo appears"
-      className="flamingo"
-     />
-    <h1 className='bruh'>bruh</h1>
-  </div>;
+  return <div className="App">bruh</div>;
 }
 
 export default App;
